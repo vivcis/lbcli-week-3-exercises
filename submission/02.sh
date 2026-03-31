0d63e6f5ab91b5b938f8dedbfee6fac192b17ac2
vivcis <ceciliaorji.co@gmail.com>
2026-03-31 14:50:51 +0100

    create native segwith address

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,4 @@
 # Create a native segwit address and get the public key from the address.
+#!bin/bash
+
+bitcoin-cli -named getaddressinfo address=$(bitcoin-cli -named getnewaddress address_type=bech32 | jq -r .result) | jq -r .result.pubkey    
