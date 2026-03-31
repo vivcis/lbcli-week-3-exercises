784dc220e3c886ee4fb454c0945f58d2eba183ab
vivcis <ceciliaorji.co@gmail.com>
2026-03-31 22:28:48 +0100

    update scripts

--- submission/05.sh
+++ submission/05.sh
@@ -6,8 +6,18 @@
 
 #!/bin/bash
 
-addressee="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
-amount="20000000"
-utxo="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+RAWTX="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
 
-bitcoin-cli -regtest walletcreatefundedpsbt "[]" "[{\"txid\":\"$(echo $utxo | cut -d' ' -f1)\",\"vout\":$(echo $utxo | cut -d' ' -f2),\"scriptPubKey\":\"$(echo $utxo | cut -d' ' -f3)\",\"amount\":$(echo $utxo | cut -d' ' -f4)\"}]"]" "[{\"${addressee}\":${amount}}]" 0
+OUTPUT_ADDRESS="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
+AMOUNT="20000000"
+
+DECODED=$(bitcoin-cli -regtest decoderawtransaction "$RAWTX")
+
+TXID=$(echo "$DECODED" | jq -r '.txid')
+VOUT=0 
+
+PSBT=$(bitcoin-cli -regtest createpsbt \
+  "[{\"txid\":\"$TXID\",\"vout\":$VOUT}]" \
+  "{\"$OUTPUT_ADDRESS\":$(echo "scale=8; $AMOUNT/100000000" | bc)}")
+
+echo "$PSBT"
