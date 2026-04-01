3de53b4b7764c01ceb6a9610ec2d146aafaa115f
vivcis <ceciliaorji.co@gmail.com>
2026-04-01 12:34:56 +0100

    omooooo

--- submission/05.sh
+++ submission/05.sh
@@ -6,27 +6,11 @@
 
 #!/bin/bash
 
-rawtx="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+bitcoin-cli -testnet createpsbt \
+  '[{"txid":"23c19f37d4e92e9a115aab86e4edc1b92a51add4e0ed0034bb166314dde50e16","vout":0},{"txid":"23c19f37d4e92e9a115aab86e4edc1b92a51add4e0ed0034bb166314dde50e16","vout":1}]' \
+  '[{"2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP":0.20000000},{"<change_address>":0.03678108}]'
 
-output_address="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
-amount_sats=20000000
+bitcoin-cli -testnet walletprocesspsbt "<psbt_base64>"
 
-txid=$(python3 << 'PYTHON'
-import hashlib
-rawtx = "01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
-data = bytes.fromhex(rawtx)
-hash1 = hashlib.sha256(data).digest()
-hash2 = hashlib.sha256(hash1).digest()
-# Little-endian txid (reversed)
-txid = hash2[::-1].hex()
-print(txid)
-PYTHON
-)
-
-amount_btc=$(awk "BEGIN {printf \"%.8f\", $amount_sats / 100000000}")
-
-psbt=$(bitcoin-cli -regtest createpsbt \
-  "[{\"txid\":\"$txid\",\"vout\":0},{\"txid\":\"$txid\",\"vout\":1}]" \
-  "{\"$output_address\":$amount_btc}")
-
-echo "$psbt"
+bitcoin-cli -testnet analyzepsbt "<psbt_base64>"
+bitcoin-cli -testnet decodepsbt "<psbt_base64>"
